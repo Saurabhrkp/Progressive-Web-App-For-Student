6249527b50a2f664a9d17065ab9b8a6e842457ab
SAURABH PATEL <saurabhpatel230@gmail.com>
2019-04-29 15:25:05 +0530

    removed locals.moments

--- app.js
+++ app.js
@@ -71,9 +71,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Moment Locals
-app.locals.moment = require('moment');
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
